973c0470257b2f11fda53aed8f4525e4d030bdb0
Jakob Bellamy <jakbellamy@gmail.com>
2018-11-04 23:34:58 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,13 +1,13 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  [*1..11].sample
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{card total}"
 end
 
 def prompt_user
